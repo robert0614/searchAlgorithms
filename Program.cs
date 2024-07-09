7930934a7f446415ffff31c3a5655d8439683f9d
robert0614 <robert.mella@stmartin.com>
2024-07-09 13:35:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace searchAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
